4a11a032e8401f5600334b06c7a299e7c29a63fa
code-for-coffee <jtraverjr@gmail.com>
2016-10-19 11:08:53 -0500

    we are fucking champs

---
+++ app.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var path 	= require('path');
+var app 	= express();
+var port 	= 3000;
+
+
+
+app.listen(process.env.PORT || port);
+console.log("Server has started");
